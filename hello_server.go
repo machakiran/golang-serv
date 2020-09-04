ad992da3c268b6a2652c51c031c49441ea45b19a
murali883 <p.murali730@gmail.com>
2020-09-04 21:26:01 +0530

    code change2

--- hello_server.go
+++ hello_server.go
@@ -18,7 +18,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	query := r.URL.Query()
 	name := query.Get("name")
 	if name == "" {
-		name = "Welcome to ExclcloudHQ org"
+		name = "Welcome to ExclcloudHQ"
 	}
 	log.Printf("Received request for %s\n", name)
 	w.Write([]byte(fmt.Sprintf("Hello, %s\n", name)))
